2750030720f35e7bbfdff2d8cf3ee68b68f3b5f6
Nethmika4881 <nethmikakumarasiri@gmail.com>
2025-10-01 16:42:57 +0530

    somehow static version has done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topic />
+      <div className="main">
+        <GetInput topic="From" currency="" key="from" />
+        <GetInput topic="To" currency="" key="to" />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Topic() {
+  return (
+    <div className="topic">
+      <h3>Curreny Converter</h3>
+      <h5>Convert between world currencies</h5>
+    </div>
+  );
+}
+
+function GetInput({ topic }) {
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("USD");
+  const [currenciesList, setCurrenciesList] = useState([]);
+  const [currenciesDes, setCurrenciesListDes] = useState([]);
+  useEffect(function () {
+    const getCurrencies = async function () {
+      const res = await fetch(
+        "      https://api.frankfurter.dev/v1/currencies"
+      );
+      const resJson = await res.json();
+      const currencies = Object.keys(resJson);
+      const currenciesDes = Object.values(resJson);
+
+      setCurrenciesList(currencies);
+      setCurrenciesListDes(currenciesDes);
+    };
+    getCurrencies();
+  }, []);
+
+  return (
+    <div className="getInput">
+      <p>{topic}</p>
+      <form className="form">
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        ></input>
+
+        <select
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="selection"
+        >
+          {currenciesList.map((curr) => (
+            <option value={curr} key={curr}>
+              {curr}
+            </option>
+          ))}
+        </select>
+      </form>
+      <p>{currenciesDes.at(currenciesList.indexOf(currency))}</p>
+    </div>
+  );
+}
